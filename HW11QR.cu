83f4198099f949af130c033cc5fa7d1b2afbe838
aronmack <amack1022@gmail.com>
2024-09-26 09:51:30 -0500

    HW11QR.cu

--- HW11QR.cu
+++ HW11QR.cu
@@ -1,4 +1,4 @@
-//nvcc QuadraticRepulsionBroken.cu -o bounce -lglut -lm -lGLU -lGL																													
+//nvcc HW11QR.cu -o bounce -lglut -lm -lGLU -lGL																													
 //To stop hit "control c" in the window you launched it from.
 #include <iostream>
 #include <fstream>
